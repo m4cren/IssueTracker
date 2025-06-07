eb0b3791051a832a7bfbae12c07b2e41353a7617
m4cren <renrenofficial@gmail.com>
2025-06-07 22:32:57 +0800

    build the navbar with styled active links

---
+++ app/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+const NavBar = () => {
+   const links = [
+      {
+         href: "/",
+         lbl: "Dashboard",
+      },
+      {
+         href: "/issues",
+         lbl: "Issues",
+      },
+   ];
+   const currentPath = usePathname();
+   return (
+      <nav className="flex flex-row gap-8 px-6 py-4 items-center border-b-1 border-[#2c2c2c35]">
+         <Link href={"/"}>
+            <AiFillBug size={20} color="#2c2c2c" />
+         </Link>
+         <ul className="flex flex-row gap-8">
+            {links.map(({ href, lbl }, index) => (
+               <Link
+                  href={href}
+                  key={index}
+                  className={classnames({
+                     "text-zinc-900 underline font-bold": currentPath === href,
+                     "text-zinc-500": currentPath !== href,
+                     "font-medium hover:text-zinc-700 transition duration-200":
+                        true,
+                  })}
+               >
+                  {lbl}
+               </Link>
+            ))}
+         </ul>
+      </nav>
+   );
+};
+
+export default NavBar;
